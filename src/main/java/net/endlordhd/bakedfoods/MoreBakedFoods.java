924834aed288f635b5148de68acc4a016c5b9a5c
EndCod3r <endlord15@proton.me>
2025-02-12 19:17:34 -0900

    Initial commit

---
+++ src/main/java/net/endlordhd/bakedfoods/MoreBakedFoods.java
@@ -0,0 +1,16 @@
+package net.endlordhd.bakedfoods;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreBakedFoods implements ModInitializer {
+	public static final String MOD_ID = "baked-foods";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/endlordhd/bakedfoods/MoreBakedFoodsClient.java
@@ -0,0 +1,10 @@
+package net.endlordhd.bakedfoods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreBakedFoodsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
